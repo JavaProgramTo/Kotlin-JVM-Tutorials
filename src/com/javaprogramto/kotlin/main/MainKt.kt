ad8187af22a5a7e4d9e95df1f3338afdf320750d
java-w3schools <venkivenki4b6@gmail.com>
2020-04-30 00:35:33 +0530

    Kotlin Main Function - fun main(args: Array<String>) Examples

---
+++ src/com/javaprogramto/kotlin/main/MainKt.kt
@@ -0,0 +1,16 @@
+package com.javaprogramto.kotlin.main
+
+import java.util.*
+
+//fun main() {
+//
+//    print("Hello world")
+//}
+
+
+fun main(args: Array<String>) {
+
+    println("Hello world with string[] array as argument")
+    println("argumens : "+Arrays.toString(args))
+
+}
